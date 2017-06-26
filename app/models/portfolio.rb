1be341bbdbdc2b80ea76ea05bc028d3ec9639aad
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-06-26 15:35:25 +0000

    Implemented form configuration for nested attributes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -17,6 +17,6 @@ class Portfolio < ApplicationRecord
     
     def set_defaults
         self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-        self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+        self.thumb_image ||= Placeholder.image_generator(height: '350', width: '150')
     end
 end
